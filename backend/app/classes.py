3129828881e21c10fd800d393c6cdd276797a2c3
Miky <vyasmiky@gmail.com>
2023-08-31 17:41:37 -0400

    formatted using black

--- backend/app/classes.py
+++ backend/app/classes.py
@@ -1,38 +1,43 @@
-from torch import nn,flatten
+from torch import nn, flatten
 import torch
+
+
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(1,6,25,padding="same"),
-                                nn.ReLU(),
-                                nn.MaxPool2d(2,2))
-        self.conv2 = nn.Sequential(nn.Conv2d(6,9,15,padding="same"),
-                                nn.ReLU(),
-                                nn.MaxPool2d(2,2))
-        self.conv3 = nn.Sequential(nn.Conv2d(9,12,5,padding="same"),
-                                nn.ReLU(),
-                                nn.MaxPool2d(2,2))
-        self.fc = nn.Sequential(nn.Linear(108,100),
-                                nn.ReLU(),
-                                nn.Linear(100,80),
-                                nn.ReLU(),
-                                nn.Linear(80,10))
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 6, 25, padding="same"), nn.ReLU(), nn.MaxPool2d(2, 2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(6, 9, 15, padding="same"), nn.ReLU(), nn.MaxPool2d(2, 2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(9, 12, 5, padding="same"), nn.ReLU(), nn.MaxPool2d(2, 2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(108, 100),
+            nn.ReLU(),
+            nn.Linear(100, 80),
+            nn.ReLU(),
+            nn.Linear(80, 10),
+        )
 
     def forward(self, x):
-        x = x.reshape(-1,1,28,28)
+        x = x.reshape(-1, 1, 28, 28)
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = flatten(x,1)
-        x= self.fc(x)
+        x = flatten(x, 1)
+        x = self.fc(x)
         return x
+
 
 def load_model():
     mod = Model()
     checkpoint = torch.load("model2.ckpt")
     modw = checkpoint["state_dict"]
     for k in list(modw):
-        modw[k.replace("model.","")] = modw.pop(k)
+        modw[k.replace("model.", "")] = modw.pop(k)
     print("loaded correctly")
     mod.load_state_dict(checkpoint["state_dict"])
     print("evaluating")
--- backend/app/main.py
+++ backend/app/main.py
@@ -29,35 +29,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class Imagestr(BaseModel):
     image: str
 
+
 @app.post("/predict")
-def predict(imgstr:Imagestr):
-    imgbytes:str = imgstr.dict()["image"].split(",")[-1]
-    img = Image.open(io.BytesIO(base64.decodebytes(bytes(imgbytes,"utf-8"))))
-    tensortransforms = T.Compose([
-        T.Resize((28,28)),
-        T.ToTensor()
-    ])
-    x_test:torch.Tensor = tensortransforms(img)
-    x_test = x_test[-1,:,:]
+def predict(imgstr: Imagestr):
+    imgbytes: str = imgstr.dict()["image"].split(",")[-1]
+    img = Image.open(io.BytesIO(base64.decodebytes(bytes(imgbytes, "utf-8"))))
+    tensortransforms = T.Compose([T.Resize((28, 28)), T.ToTensor()])
+    x_test: torch.Tensor = tensortransforms(img)
+    x_test = x_test[-1, :, :]
     mod = Model()
     checkpoint = torch.load("model2.ckpt")
     modw = checkpoint["state_dict"]
     for k in list(modw):
-        modw[k.replace("model.","")] = modw.pop(k)
+        modw[k.replace("model.", "")] = modw.pop(k)
     mod.load_state_dict(checkpoint["state_dict"])
     mod.eval()
 
     with torch.no_grad():
-        y_hat = torch.nn.functional.softmax(mod(x_test),dim=1)
+        y_hat = torch.nn.functional.softmax(mod(x_test), dim=1)
         print(y_hat)
         prediction = torch.argmax(y_hat).item()
         print(prediction)
     lb = joblib.load("labelencoder.joblib")
     predclass = lb.inverse_transform([prediction])
-    return {"pred":predclass[0]}
+    return {"pred": predclass[0]}
 
 
 if __name__ == "__main__":
